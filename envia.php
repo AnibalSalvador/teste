517ac058cc4139e20a2e765ffad946a4108d6f58
AnibalSalvador <129404350+AnibalSalvador@users.noreply.github.com>
2023-03-30 15:58:50 +0200

    Delete envia.php

--- envia.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$to='anibalmauricio76@gmail.com';
-$subject='teste';
-$message='teste';
-$headers='from: anibalsalvador76@gmail.com';
-
-mail($to,$subject,$message,$headers);
-print "enviado";
-?>
