debf99d4952b53b4094c572e0916833efce24dd0
AnibalSalvador <129404350+AnibalSalvador@users.noreply.github.com>
2023-03-30 15:54:49 +0200

    Create envia.php

---
+++ envia.php
@@ -0,0 +1,9 @@
+<?php 
+$to='anibalmauricio76@gmail.com';
+$subject='teste';
+$message='teste';
+$headers='from: anibalsalvador76@gmail.com';
+
+mail($to,$subject,$message,$headers);
+print "enviado";
+?>
